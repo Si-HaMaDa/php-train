06758a26cdfbed043cb3f75feffa3a54fdf19d9d
Si HaMaDa <nrookman_27@yahoo.com>
2021-11-28 01:24:26 +0200

    rr

--- index.php
+++ index.php
@@ -154,7 +154,6 @@
         </div>
     </footer>
 
-
     <script src="https://getbootstrap.com/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 
